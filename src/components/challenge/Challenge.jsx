fb9a005be7a09a7b37272952a66485c9c5b322b9
joykhera <joykhera@gmail.com>
2022-09-30 03:28:30 -0400

    changled Challenge

--- src/components/challenge/Challenge.jsx
+++ src/components/challenge/Challenge.jsx
@@ -4,30 +4,16 @@ import firebaseConfig from '../../firebase'
 import { doc, setDoc, getDocs, collection, query, where } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebase from 'firebase/compat/app';
 
 export default function Challenge() {
     const db = getFirestore(initializeApp(firebaseConfig));
-    const firestore = firebase.firestore();
 
     const [challenge, setChallenge] = useState('');
     const todaysDate = new Date().toLocaleDateString();
     const todaysChallengeRef = query(collection(db, "dailyChallenge"), where("date", "==", todaysDate));
 
-    // firestore.collection('dailyChallenge').doc(uid).get().then((docSnapshot) => {
-    //         if (!docSnapshot.exists) {
-    //             firestore.collection('users').doc(uid).set({
-    //                 displayName,
-    //                 email,
-    //                 photoURL,
-    //                 subscribed: false
-    //             })
-    //         }
-    //     });
-
     (async () => {
         const todaysChallengeSnap = await getDocs(todaysChallengeRef);
-        console.log(todaysChallengeSnap.empty)
 
         if (todaysChallengeSnap.empty) {
             // let size = 0;
@@ -37,13 +23,11 @@ export default function Challenge() {
             // console.log(size)
             let size = 11
             const randomNum = Math.floor(Math.random() * size)
-            console.log(randomNum)
             const docRef = query(collection(db, "challenges"), where("id", "==", randomNum));
             const docSnap = await getDocs(docRef)
             let data;
-            docSnap.forEach((doc) => {
-                data = doc.data()
-            });
+
+            docSnap.forEach((doc) => data = doc.data());
 
             await setDoc(doc(db, "dailyChallenge", todaysDate.replaceAll('/', '-')), {
                 id: randomNum,
